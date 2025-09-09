53158982d1557a876fc1bfaba034678d061e8ddb
Siddharth <siddharth.tandon1@gmail.com>
2025-09-09 04:08:16 +0000

    Committing ddp version of inference

--- whisper_finetuning/run_inference.sh
+++ whisper_finetuning/run_inference.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-accelerate launch src/inference.py \
-    --model_name_or_path output/checkpoint-1000/
+# python src/inference.py --model_name_or_path output/checkpoint-1000/
+
+# Use Zero 3 config setting as used during training
+# accelerate launch --config_file deep_speed_config.yaml src/inference_distributed.py \
+#     --model_name_or_path output/checkpoint-1000/
+
+# Use DDP setting
+accelerate launch --multi_gpu src/inference.py --model_name_or_path output/checkpoint-1000/ > output_inf.log 2>&1
